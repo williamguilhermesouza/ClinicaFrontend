b1650a5a94304ec5d9bcc2bb66ed259b959fc0dc
williamguilhermesouza <williamguilhermesouza@gmail.com>
2020-11-05 20:24:45 -0300

    fixed component routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
 
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent },
+    ]),
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
