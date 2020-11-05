6fc8c6f3e967a58a021da30a5796390aa8ef7698
williamguilhermesouza <williamguilhermesouza@gmail.com>
2020-11-05 15:12:57 -0300

    trying to fix routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    HomeComponent,
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule,
