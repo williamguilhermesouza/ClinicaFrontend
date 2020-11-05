55d1916a21c0152e7df8c15a70af91aa3300a45b
williamguilhermesouza <williamguilhermesouza@gmail.com>
2020-11-04 23:21:59 -0300

    added router-outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
     { path: '/home', component: HomeComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
 @NgModule({
