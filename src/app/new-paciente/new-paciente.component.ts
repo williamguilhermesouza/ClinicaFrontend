d0fdd263c16b9f2962b690323a4f6d341d6389e4
williamguilhermesouza <williamguilhermesouza@gmail.com>
2020-11-06 16:17:09 -0300

    working on new patient form

--- src/app/new-paciente/new-paciente.component.ts
+++ src/app/new-paciente/new-paciente.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-new-paciente',
@@ -11,5 +12,8 @@ export class NewPacienteComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  id = new FormControl('');
+  
 
 }
