2ed8f1a170477b1ed4b9f98a70d1e67645d1ff66
williamguilhermesouza <williamguilhermesouza@gmail.com>
2020-12-31 09:28:08 -0300

    completed http service

--- src/services/pacientes.service.ts
+++ src/services/pacientes.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 
 import { Paciente } from '../models/paciente.model';
 
@@ -10,5 +11,27 @@ import { Paciente } from '../models/paciente.model';
 export class PacientesService {
   private readonly Backend = `${environment.Backend}`;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPacientes(): Observable<Paciente[]> {
+      return this.http.get<Paciente[]>(`${this.Backend}pacientes`);
+  }
+
+  getPacientesByName(paciente: string): Observable<Paciente[]> {
+      return this.http.post<Paciente[]>(`${this.Backend}pacientes/findbyname`, {nome: paciente});
+  }
+
+  createPaciente(paciente: Paciente): Observable<Paciente> {
+      return this.http.post<Paciente>(`${this.Backend}pacientes/create`, paciente);
+  }
+
+  deletePaciente(id: number): void {
+      this.http.delete(`${this.Backend}pacientes/${id}`);
+  }
+
+  updatePaciente(id: number): Observable<Paciente> {
+      return this.http.get<Paciente>(`${this.Backend}pacientes/${id}`);
+  }
 }
+
+    
